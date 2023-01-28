9fc64fb3d83c41799857ccf0d6cd9fa299ea34ed
az1nn <alansa015@gmail.com>
2023-01-28 13:56:19 -0300

    create api/user

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -1,25 +1,43 @@
+import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { User } from '../../../interfaces'
 
-export default function userHandler(
-  req: NextApiRequest,
-  res: NextApiResponse<User>
-) {
-  const { query, method } = req
-  const id = parseInt(query.id as string, 10)
-  const name = query.name as string
+const prisma = new PrismaClient()
 
-  switch (method) {
-    case 'GET':
-      // Get data from your database
-      res.status(200).json({ id, name: `User ${id}` })
-      break
-    case 'PUT':
-      // Update or create data in your database
-      res.status(200).json({ id, name: name || `User ${id}` })
-      break
-    default:
-      res.setHeader('Allow', ['GET', 'PUT'])
-      res.status(405).end(`Method ${method} Not Allowed`)
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const query = _req.query
+  const {first_name, last_name, participation} = _req.body
+  const { id } = query
+  const NumId = parseInt((id as string))
+  const numP = parseInt(participation)
+  if(_req.method === "GET") {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: NumId,
+        },
+      })
+      if(!!user) res.status(200).json(user)
+      if(user === null) res.status(404).json({ message: "Not Found" })
+  }
+  if(_req.method === "POST") {
+    try {
+      await prisma.user.update({
+        where: {
+          id: NumId,
+        },
+        data: {
+          first_name,
+          last_name,
+          participation: numP 
+        }
+      })
+      res.status(200).json({ message: 'User Updated' })
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ message: error })
+    }
   }
 }
