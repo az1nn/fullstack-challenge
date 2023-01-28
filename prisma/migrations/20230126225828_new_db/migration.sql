6d3c631081b4a43ea104af55895972526d4b365f
az1nn <alansa015@gmail.com>
2023-01-28 13:55:41 -0300

    add config files

---
+++ prisma/migrations/20230126225828_new_db/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "participation" INTEGER NOT NULL
+);
